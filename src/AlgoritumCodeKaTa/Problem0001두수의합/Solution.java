33da0ab4468de10ebae19dacfa425102916fc4c9
HyunSu Im <colorlhs@gmail.com>
2024-10-17 09:25:43 +0900

    second commit

---
+++ src/AlgoritumCodeKaTa/Problem0001두수의합/Solution.java
@@ -0,0 +1,8 @@
+package AlgoritumCodeKaTa.Problem0001두수의합;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
---
+++ src/AlgoritumCodeKaTa/Problem0002두수의곱/Solution.java
@@ -0,0 +1,8 @@
+package AlgoritumCodeKaTa.Problem0002두수의곱;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
